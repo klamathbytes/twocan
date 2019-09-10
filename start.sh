0ba3c4fc4e7ed0e7d939def7bfccb5ae35fac42a
Josh Reichardt <josh.reichardt@gmail.com>
2019-09-09 21:13:37 -0700

    Put parsed data into database

--- start.sh
+++ start.sh
@@ -6,14 +6,20 @@ set -eux
 cat > .env <<EOF
 PGDATABASE=twocan
 PGUSER=postgres
+PGPASSWORD=twocan
 PGPORT=5432
 EOF
+
+# shellcheck disable=1091
+source .env
 
 echo "Starting database"
 docker-compose up -d
 while ! nc -z localhost 5432; do sleep 1; done
 
-echo "Populating database"
-if [[ ! -f members.json ]]; then
-    node index.js > members.json
-fi
+echo "Populating database, this may take some time"
+time node index.js
+
+# TODO Check if the records were inserted
+# PGPASSWORD="$PGPASSWORD" psql -h localhost -U "$PGUSER" twocan
+# select count(*) from individual;
